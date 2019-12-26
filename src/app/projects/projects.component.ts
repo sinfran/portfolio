1bd87052c1cfbfa842c512e119b6f39c902fb9ac
Frances Sin <frances.sin31@gmail.com>
2019-12-26 12:28:20 -0800

    link updates

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-projects',
@@ -9,7 +7,7 @@ import { ModalComponent } from '../modal/modal.component';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor(public matDialog: MatDialog) {
+  constructor() {
    }
 
   ngOnInit() {
@@ -19,16 +17,6 @@ export class ProjectsComponent implements OnInit {
     console.log('function called');
   }
 
-  openModal() {
-    const dialogConfig = new MatDialogConfig();
-    dialogConfig.disableClose = false;
-    dialogConfig.id = "modal-component";
-    dialogConfig.height = "100%";
-    dialogConfig.width = "75%";
-    dialogConfig.maxHeight = '100%';
-    dialogConfig.panelClass = 'custom-dialog-container';
-    const modalDialog = this.matDialog.open(ModalComponent, dialogConfig);
-  }
 
 
 }
