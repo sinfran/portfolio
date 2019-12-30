15761eac05a37d4265ac34440c9e1ef5694fd105
Frances Sin <frances.sin31@gmail.com>
2019-12-30 11:26:16 -0800

    add image animations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('slowFade', [
+      state('in', style({ 
+        opacity: 1
+      })),
+      transition(':enter', [
+        style({
+          opacity: 0
+        }),
+        animate('700ms 200ms ease-in')
+      ])
+    ])
+]
 })
 export class HomeComponent implements OnInit {
 
   constructor() { }
+
 
   ngOnInit() {
   }
